efa919f46c9f97f7987ca9bc572cd3dbf43a0b80
anna <annayth@gmail.com>
2021-11-22 07:29:17 -0500

    application and trainer controller

--- pokeMTA/app/controllers/application_controller.rb
+++ pokeMTA/app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+    # CELLL
+
+    helper_method :current_trainer, :logged_in?
+
+    def current_trainer 
+        @current_trainer ||= Trainer.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_trainer
+    end
+
+    def login(trainer)
+        session[:session_token] = trainer.reset_session_token!
+    end
+
+    def logout
+        current_trainer.reset_session_token! if logged_in?
+        session[:session_token] = nil 
+        redirect_to new_session_url
+    end
+
+    # ENSURRE
+
+    def require_logged_in
+        redirect_to new_session_url unless logged_in?
+    end
+
+    def require_logged_out
+        redirect_to users_url if logged_in?
+    end
 end
--- pokeMTA/app/controllers/trainers_controller.rb
+++ pokeMTA/app/controllers/trainers_controller.rb
@@ -1,3 +1,45 @@
 class TrainersController < ApplicationController
+    before_action :require_logged_in, only: [:show, :index]
+    
+    def new 
+        @trainer = Trainer.new
+        render :new
+    end
 
+    def create 
+        @trainer = Trainer.new(trainer_params)
+
+        if @user.save
+            login(@trainer)
+            redirect_to trainers_url
+        else 
+            flash.now[:errors] = ["Password is too short (minimum is 6 characters)"]
+            render :new
+        end
+    end
+
+    def index 
+        @trainers = Trainer.all 
+        render :index
+    end
+
+    def show 
+        @trainer = Trainer.find(params[:id])
+        # redirect_to user_url(@user)
+        render :show
+    end
+
+    private 
+    def trainer_params
+        params.require(:trainer).permit(:name, :password)
+    end
 end
+
+# create_table "trainers", force: :cascade do |t|
+#     t.string "name", null: false
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.string "session_token", null: false
+#     t.string "password_digest", null: false
+#     t.index ["name"], name: "index_trainers_on_name"
+#   end
